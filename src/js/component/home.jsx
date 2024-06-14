41cbad0df4f7d16f23e7b79cbb4f5e3455844e45
Styoks <113370103+Styoks@users.noreply.github.com>
2024-06-14 18:10:45 +0000

    final project (missing to empty the input value)

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,9 +6,18 @@ const Home = () => {
 	const [list, setList] = useState([])
 	const [name, setName] = useState("Styoks")
 	const [showModal, setShowModal] = useState(true);
+	const [isDivVisible, setIsDivVisible]= useState(true)
+	const [user, setUser] = useState("")
 
-	const handleShowModal = () => setShowModal(true);
-	const handleCloseModal = () => setShowModal(false);
+	const handleShowModal = () => {
+		setShowModal(true)
+		setIsDivVisible(true)
+	};
+
+	const handleCloseModal = () => {
+		setShowModal(false)
+		setIsDivVisible(false)
+	};
 
 	useEffect(()=> {
 		getData()
@@ -72,13 +81,24 @@ const Home = () => {
 		}
 	}
 
-	function changeUser(event) {
+	function saveUser (event) {
+		setUser(event.target.value)
 		if (event.keyCode === 13 && event.target.value !== ""){
-			setName(event.target.value)
-			event.target.value = ""
-			handleCloseModal()
+			changeUser()
 		}
 	} 
+
+	function changeUser () {
+		setName(user)
+		setIsDivVisible(false)
+		handleCloseModal()
+	}
+
+	function isEnter (event) {
+		if (event.keyCode === 13) {
+			changeUser()
+		}
+	}
 
 	async function clearUser () {
 		await Promise.all(list.map(async(item)=>{
@@ -89,46 +109,49 @@ const Home = () => {
 		
 		setList([])
 	}
-
-	const finalMessage = 
-	(list.length > 0) ? `${list.length} items left` : "No tasks, add a task"
 	
 	return (
-		<div className="container d-flex justify-content-center align-items-center flex-column p-5 rounded-5">
-			<h1 className="display-1 mb-5">todos</h1>
+		<div className="container d-flex justify-content-center align-items-center flex-column p-5 rounded-5 shadow">
+			<p className="display-1 mb-2">todos</p>
+			<p className="display-5 mb-5">{name}'s user</p>
 			<div className="w-100">
 				<input className="w-100 input-group-text" type="text" onKeyDown={enterTodo} />
-				<ul className="list-group mt-5">
+				<ul className="list-group mt-5 shadow">
 				{ list.map((item, index) => {
 					return <li key={index} id={item.id} className="list-group-item justify-content-between d-flex liColor">{item.label}<button className="closeButton rounded-3" type="button" onClick={()=>removeTask(item.id)}><strong>x</strong></button></li>
-				}) }
-				<li className="list-group-item liColor">{finalMessage}</li>
+				} ) }
+				<li className="list-group-item liColor">{(list.length > 0) ? `${list.length} items left` : "No tasks, add a task"}</li>
 				</ul>
 			</div>
-			<button className="btn btn-primary mt-2" onClick={clearUser}>Clear User</button>
-			<button type="button" className="btn btn-primary" data-bs-toggle="modal" onClick={handleShowModal} data-bs-target="#exampleModal">
-				Change user
-			</button>
+			<div className="d-flex gap-5">
+				<button className="btn btn-danger mt-5" onClick={clearUser}>Clear User</button>
+				<button type="button" className="btn btn-success mt-5" onClick={handleShowModal}>
+					Change user
+				</button>
+			</div>
 			<div className={`modal fade ${showModal ? "show" : ""}`} style={{ display: showModal ? "block" : "none" }} id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
 				<div className="modal-dialog">
 					<div className="modal-content">
-					<div className="modal-header">
-						<h1 className="modal-title fs-5" id="exampleModalLabel">Put your user below</h1>
-						<button type="button" className="btn-close" data-bs-dismiss="modal" onClick={handleCloseModal} aria-label="Close"></button>
-					</div>
-					<div className="modal-body">
-						<input type="text" onKeyDown={changeUser} className="w-100" placeholder="If the user does not exist it will create it for you"/>
-					</div>
-					<div className="modal-footer">
-						<button type="button" className="btn btn-secondary" onClick={handleCloseModal} data-bs-dismiss="modal">Close</button>
-						<button type="button" className="btn btn-primary" onClick={changeUser}>Change user!</button>
-					</div>
+						<div className="modal-header">
+							<h1 className="modal-title fs-5" id="exampleModalLabel">Put your user below</h1>
+							<button type="button" className="btn-close" data-bs-dismiss="modal" onClick={handleCloseModal} aria-label="Close"></button>
+						</div>
+						<div className="modal-body">
+							<input type="text" onChange={saveUser} onKeyDown={isEnter} className="w-100 input-group-text" placeholder="If the user does not exist it will create it for you"/>
+						</div>
+						<div className="modal-footer">
+							<button type="button" className="btn btn-secondary" onClick={handleCloseModal} >Close</button>
+							<button type="button" className="btn btn-primary" onClick={changeUser} >Change user!</button>
+						</div>
 					</div>
 				</div>
 			</div>
 			
+			{isDivVisible ? (
+				<div className="modal-backdrop fade show"></div>
+			  ) : null}
 		</div>
-		
+
 	);
 };
 
